4c2dfff2e8dce5ebccbd922abcfe2011788d027e
ubiot-alejandro <41581013+ubiot-alejandro@users.noreply.github.com>
2019-04-17 09:57:17 -0400

    Update main.py
    Now the function can show the warranty satus

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 '''
 Ésta función permite hacer el Query a la DB en Firestore.
 Puede configurarse para hacer query a toda la collección
-o para hacer query a una parte determinada.
+o para hacer query a una parte determinada y mostrar el 
+estado activo o inactivo de la garantía.
 
 This function allows querying the Firestore DB.
 May be configured to querying all collection or
-just a part of it.
+just a part of it and show an active or inactive
+state of warranty.
 '''
 
 import firebase_admin
 import google.cloud
 from firebase_admin import credentials, firestore
+from datetime import datetime
 
 cred = credentials.Certificate("./ServiceAccountKey.json")
 app = firebase_admin.initialize_app(cred)
@@ -18,11 +21,30 @@ app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 print("Initializing...")
 
-# Query against a part of the collection
-docs = db.collection(u'test').where(u'warrantyStatus', u'==', 'Active').get()
-
 #Query against all collection
-#docs = db.collection(u'test').get()
+#docs = db.collection(u'dtcDB').limit(10).get()
 
+#Query against a part of the collection
+docs = db.collection(u'dtcDB').where(u'GTIN', u'==', '10637').get()
+
+#Print specific data
 for doc in docs:
-    print(u'{} => {}'.format(doc.id, doc.to_dict()))
+    dict = doc.to_dict()
+    wdates = dict.get("warrantyDate")
+
+    wdatet = datetime.strptime(wdates, "%Y/%m/%d")  #Transfor warrantyDate from strig to timestamp
+    wdate = wdatet.date()                           #Take just the date of warrantyDate
+
+    nowt = datetime.now()                           #Timestam of today
+    datenow = nowt.date()                           #Take just the date of today
+
+    if wdate < datenow:                             #Date comparation
+        print("Your warranty is inactive")          #Inactive warrranty
+    elif wdate > datenow:
+        print("Your warranty is active")            #Active warrranty
+    elif wdate == datenow:
+        print("Your warranty ends today")           #Warranty ends today
+
+#Print all data
+#for doc in docs:
+    #print("Your id invoice is " '"{}" \nAnd your data is: {}'.format(doc.id, doc.to_dict()))
